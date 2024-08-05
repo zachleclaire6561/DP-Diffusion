fixing normalization

dp_diffusion.py
@@ -20,7 +20,7 @@ def training_loop(model, optimizer, scheduler, hyperparams, dataloader, save_pat
     
     losses = []
     
-    itter = 0
+    itter = hyperparams["load iteration"]
     while True:
         for (step, batch) in enumerate(dataloader):
             checkpoint_path = f"{save_path}{itter}"
@@ -40,11 +40,6 @@ def training_loop(model, optimizer, scheduler, hyperparams, dataloader, save_pat
             loss = p_losses(model, batch, t, hyperparams)
             losses.append(loss.detach().cpu())
 
-            if itter != 0 and itter % 5 == 0:
-                print(f"itteration {itter} Loss:", loss.item())
-
-                # save losses
-                np.savetxt(f"{checkpoint_path}", np.array(losses), delimiter=",")
 
             loss.backward()
             optimizer.step()
@@ -52,6 +47,9 @@ def training_loop(model, optimizer, scheduler, hyperparams, dataloader, save_pat
             if hyperparams["learning schedule"]:
                 scheduler.step()
             itter += 1
+    
+            if itter % 5 == 0: 
+                 print((f"itteration {itter} Loss:", loss.item()))
 
             # save checkpoints
             if itter % save_and_sample_every == 0:
@@ -64,13 +62,14 @@ def training_loop(model, optimizer, scheduler, hyperparams, dataloader, save_pat
                     'loss': loss},
                     checkpoint_path, 
                     file_name)
-                
+
+                np.savetxt(f"{checkpoint_path}/loss.npy", np.array(losses), delimiter=",")  
                 # save some samples
  
 
 if __name__ == '__main__':
 
-   # arg parse stuff here
+    # arg parse stuff here
     
 
     # load and add model parameters
@@ -124,7 +123,8 @@ if __name__ == '__main__':
     # EMA model weights
     model = AveragedModel(model)
 
-    # TODO: load pretrained model here for other examples
+    if hyperparams["load"]:
+        model.load_state_dict(torch.load(hyperparams["load file"])["model_state_dict"])
 
     optimizer = Adam(model.parameters(), lr=hyperparams["learning rate"])
     if hyperparams["learning schedule"]:

src/util.py
@@ -107,6 +107,7 @@ def load_npy(file):
 class ImageNet(Dataset):
     def __init__(self, data_dir):
         self.samples = None
+        #self.transform = Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
         for entry in os.listdir(data_dir):
             batch_file = os.path.join(data_dir, entry)
             datafile = unpickle(batch_file)["data"]
@@ -126,7 +127,9 @@ class ImageNet(Dataset):
             return self.samples.shape[0]
 
     def __getitem__(self, idx):
-        return self.samples[idx]
+        return (2*self.samples[idx].to(torch.float32)/255)-1
+
+
 
 def save_file(object, path, file_name):
     if not os.path.exists(path):
@@ -151,7 +154,9 @@ def get_augmult_transform(augmult = 0, image_shape = (32,32), random_crop = True
             transform_list.append(RandomHorizontalFlip(p = 0.5))
         #if random_color:
         #    transform_list.append()
-    transform_list.normalize()
+    
+
+    transform_list.append(Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]))
         
     return Compose(transform_list)
 